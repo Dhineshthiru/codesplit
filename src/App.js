709924095163c714780a3566b8a31c841d12ed92
Dhinesh T <dhinesh@dorustree.in>
2023-03-29 19:05:39 +0530

    Codesplit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
+import CalcErrorBoundary from './CalcErrorBoundary';
+const Calc = React.lazy(() => import('./Calc'));
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<CalcErrorBoundary>
+					<React.Suspense fallback={<div>Loading...</div>}>
+						<Calc></Calc>
+					</React.Suspense>
+				</CalcErrorBoundary>
+			</header>
+		</div>
+	);
 }
 
 export default App;
